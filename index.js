2ddb84ef43a6ace91652d205f2fd14e5bd73adbc
Noah Nyabwana <noahnyabwana@gmail.com>
2018-01-18 11:58:17 +0300

    added functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 
+let token = "EAAcIh1VVeskBAIQIEIxcJHfVdErwkTf3hUlsLG1iT7ZBDqY9gdFc3bajZA3zHnd8PF0PHT9ju4UQfgaPZBpWzdJ3IzWzi4ydwhWhVw3ZCJszW6D8ktdFa5kF2FkWo1ZBrfU7dZCzUJBvxHYt4nEIdnDgTZAsPHR2EMePrBygNIgJQZDZD"
+
 // Initialize app
 
 const app = express()
@@ -27,6 +29,38 @@ app.get('/webhook/', function(req,res){
 	}
 	res.send("Wrong Token")
 })
+
+app.post('/webhook/', function(req, res){
+	let messaging_events = req.body.entry[0].messaging 
+	for (let i=0; i<messaging_events.length; i++){
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if(event.message && event.message.text){
+			let text = event.message.text
+			sendText(sender, "Text echo: "+text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender,text){
+	let messageData = {text:text}
+	request({
+		url: "https://graph.facebook.com/v2.11/me/messages",
+		qs: {access_token:token},
+		method: "POST",
+		json: {
+			recipient:{id:sender},
+			message: messageData
+		}
+	}, function(error,response,body){
+		if(error){
+			console.log("sending error")
+		} else if(response.body.error){
+			console.log("response body error")
+		}
+	})
+}
 
 app.listen(app.get('port'), function(){
 	console.log("Running: port")
